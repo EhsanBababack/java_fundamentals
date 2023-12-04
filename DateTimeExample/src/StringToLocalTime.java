8ec6179d152f24bf6bda3b6a358bce33fc871f25
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-04 17:49:24 +0330

    Delete DateTimeExample directory

--- DateTimeExample/src/StringToLocalTime.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalTime;
-
-public class StringToLocalTime {
-    public static void main(String[] args) {
-        LocalTime localTime = LocalTime.parse("22:15:30");
-//        localTime.of(12,30 , 50);
-        System.out.println(localTime);
-    }
-}
