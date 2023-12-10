e39c100ce21245341c99745900aa1898832a93e0
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-10 13:22:39 +0330

    String example

---
+++ string-example/src/EqualExample.java
@@ -0,0 +1,8 @@
+public class EqualExample {
+    public static void main(String[] args) {
+        String password = "lkajdaddad";
+        if (password.equals("lkajdaddad")){
+            System.out.println("WELCOME ... ");
+        }
+    }
+}
