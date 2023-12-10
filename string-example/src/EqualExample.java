3586a0c8893b83c4006c057a4baf448fd70960ee
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-10 20:24:16 +0330

    Delete string-example directory

--- string-example/src/EqualExample.java
+++
@@ -1,8 +0,0 @@
-public class EqualExample {
-    public static void main(String[] args) {
-        String password = "lkajdaddad";
-        if (password.equals("lkajdaddad")){
-            System.out.println("WELCOME ... ");
-        }
-    }
-}
