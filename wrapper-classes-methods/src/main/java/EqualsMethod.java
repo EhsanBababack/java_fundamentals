f2e646b05ee71a2b03b811aa5734fe2d6e21417d
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-18 08:51:43 +0330

    Delete wrapper-classes-methods/src/main/java directory

--- wrapper-classes-methods/src/main/java/EqualsMethod.java
+++
@@ -1,6 +0,0 @@
-public class EqualsMethod {
-    public static void main(String[] args) {
-        Double d=23.465775;
-        System.out.println(d.equals(234.567));
-    }
-}
--- wrapper-classes-methods/src/main/java/ToStringMethod.java
+++
@@ -1,7 +0,0 @@
-public class ToStringMethod {
-    public static void main(String[] args) {
-        Double d=23.465775;
-        String s = d.toString();
-        System.out.println(s);
-    }
-}
