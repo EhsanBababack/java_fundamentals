95df72def7c8108b575efc0dab47c2c002bde248
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-18 08:53:13 +0330

    wrapper classes methods

---
+++ wrapper-classes-methods/src/main/java/CompareMethod.java
@@ -0,0 +1,5 @@
+public class CompareMethod {
+    public static void main(String[] args) {
+        System.out.println(Double.compare(48.23, 12.567));
+    }
+}
---
+++ wrapper-classes-methods/src/main/java/IntValueMethod.java
@@ -0,0 +1,7 @@
+public class IntValueMethod {
+    public static void main(String[] args) {
+        Double d=23.465775;
+        int i = d.intValue();
+        System.out.println(i);
+    }
+}
