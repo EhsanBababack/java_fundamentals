a1ec6501ed517f963341e3218bf3e9ee7018feb6
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-24 15:59:23 +0330

    Delete map-create-methods directory

--- map-create-methods/src/main/java/IsEmptyMethod.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;
-
-public class IsEmptyMethod {
-    public static void main(String[] args) {
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        System.out.println(hashMap.isEmpty());
-    }
-}
