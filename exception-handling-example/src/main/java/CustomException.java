e009f5562f2b2256a31a2fc19113a74d65bee0e1
EhsanBababack <130831922+EhsanBababack@users.noreply.github.com>
2023-12-25 17:11:40 +0330

    Delete exception-handling-example directory

--- exception-handling-example/src/main/java/CustomException.java
+++
@@ -1,6 +0,0 @@
-public class CustomException extends Exception{
-    public CustomException(String message) {
-        System.out.println("This is a Exception");
-    }
-
-}
--- exception-handling-example/src/main/java/MyException.java
+++
@@ -1,5 +0,0 @@
-public class MyException extends Exception{
-    public MyException(String message) {
-        super(message);
-    }
-}
